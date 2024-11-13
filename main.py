469b4870de5e4e47f28a56a976230109d84b43e3
LaurePatera <ampatera@loyola.edu>
2024-11-13 13:26:40 -0500

    changed code

--- main.py
+++ main.py
@@ -12,30 +12,29 @@ def read_file_to_list(filename):
 
         # Return the list of data
         return name_list
-    except:
-        # Ensure the file is closed in case of an error
-        file_data.close()
-
+    except FileNotFoundError:
         # Print an error message and return an empty list
         print("Error reading file")
         return data
 
 def main ():
-    count = 0
-    while count < 3:
-        file_name = input("Enter the name of the file to read: ")
-        name_list = read_file_to_list(file_name)
+    count = 1
+    name_list = []
+    while count < 4:
+        file_name = input(f'Enter the name of file {count}:')
+        name_list += read_file_to_list(file_name)
         count += 1
     table = 1
     seat = 1
     for name in name_list:
         print('~' * 24)
-        print(f'Table {table}, Seat {seat}, {name}')
-        print('~' * 24)
+        print(f'Table {table}, Seat {seat}, {name}', '~' * 24)
         seat += 1
         if seat > 5:
             seat = 1
             table += 1
     print(f'{table} tables are needed.')
+
+main()
 
 
